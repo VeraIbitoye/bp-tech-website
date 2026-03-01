d524694b364a969c510347842e8a085fcce98e5d
Vera Ibitoye <ibitoyevera2002@gmail.com>
2026-03-01 10:11:16 -0800

    Add redirect from non-www to www

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // matches all paths
+        has: [
+          {
+            type: 'host',
+            value: 'bptechandresourcesltd.com.ng', // non-www
+          },
+        ],
+        destination: 'https://www.bptechandresourcesltd.com.ng/:path*', // redirect to www
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
