e3c56a49a23893ee942facf55e4688c65896a2b4
Vera Ibitoye <ibitoyevera2002@gmail.com>
2026-03-01 09:02:18 -0800

    Added sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://bptechandresourcesltd.com.ng",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://bptechandresourcesltd.com.ng/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://bptechandresourcesltd.com.ng/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://bptechandresourcesltd.com.ng/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
