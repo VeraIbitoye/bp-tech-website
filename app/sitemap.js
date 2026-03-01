b0889675621fb9ffa09a3fc3d94e959aa733c2a8
Vera Ibitoye <ibitoyevera2002@gmail.com>
2026-03-01 10:20:00 -0800

    Fix redirect and update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,19 +1,23 @@
 export default function sitemap() {
   return [
     {
-      url: "https://bptechandresourcesltd.com.ng",
+      url: "https://www.bptechandresourcesltd.com.ng",
       lastModified: new Date(),
     },
     {
-      url: "https://bptechandresourcesltd.com.ng/about",
+      url: "https://www.bptechandresourcesltd.com.ng/about",
       lastModified: new Date(),
     },
     {
-      url: "https://bptechandresourcesltd.com.ng/services",
+      url: "https://www.bptechandresourcesltd.com.ng/services",
       lastModified: new Date(),
     },
     {
-      url: "https://bptechandresourcesltd.com.ng/contact",
+      url: "https://www.bptechandresourcesltd.com.ng/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.bptechandresourcesltd.com.ng/projects",
       lastModified: new Date(),
     },
   ];
